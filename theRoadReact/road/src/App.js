bcc3cf46e608adc6d370f6a4089451b08c4b22d8
lromang <luis.roangarci@gmail.com>
2017-11-08 13:13:51 -0600

    ...

--- theRoadReact/road/src/App.js
+++ theRoadReact/road/src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const list = [
+  {'title'        : 'React',
+   'url'          : 'http:...',
+   'author'       : 'Jordan',
+   'num_comments' : 3,
+   'points'       : 4,
+   'ObjectID'     : 0},
+  {'title'        : 'Redux',
+   'url'          : 'http:...',
+   'author'       : 'Abramov',
+   'num_comments' : 2,
+   'points'       : 5,
+   'ObjectID'     : 1}
+]
 
 class App extends Component {
   render() {
     let hello = 'Welcome to the road to learn react!';
     return (
       <div className="App">
-        <h2>{hello}</h2>
+        {list.map(item => {
+          return <div key={item.ObjectID}>
+            <span>
+            <a href={item.url}>
+            {item.title}
+          </a>
+            </span>
+            <span>
+            {item.author}
+          </span>
+            <span>
+            {item.num_comments}
+            </span>
+          </div>;
+        })}
       </div>
     );
   }
